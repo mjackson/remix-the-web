123fed950823b864b3f627433b7be56ef6656781
Michael Jackson <mjijackson@gmail.com>
2025-02-27 09:05:26 -0800

    More work on href builder

--- packages/route-pattern/src/lib/href-builder.ts
+++ packages/route-pattern/src/lib/href-builder.ts
@@ -1,27 +1,34 @@
 import type { RoutePattern } from './route-pattern.ts';
 
+// prettier-ignore
 type HrefBuilderPattern<T extends RoutePattern> =
-  T extends RoutePattern<infer P> ? PatternWithoutOptionals<P> : never;
+  T extends RoutePattern<infer P> ?
+    ExpandOptionals<P> extends infer U ?
+      U extends string ? { source: P; output: U } : never :
+      never :
+    never;
 
 // prettier-ignore
-type PatternWithoutOptionals<T extends string> =
+type ExpandOptionals<T extends string> =
   T extends `${infer L}(${infer M})${infer R}` ?
-    `${L}${PatternWithoutOptionals<R>}` | `${L}${M}${PatternWithoutOptionals<R>}` :
-    T
+    `${L}${ExpandOptionals<R>}` | `${L}${M}${ExpandOptionals<R>}` :
+    T;
 
-type HrefBuilderParams<T extends string> = Record<string, unknown>; // TODO
-type HrefBuilderSearchParams<T extends string> = Record<string, unknown>; // TODO
+type HrefBuilderParams<T extends RoutePattern> = Record<string, T['source']>; // TODO
+type HrefBuilderSearchParams<T extends RoutePattern> = Record<string, unknown>; // TODO
 
-interface HrefBuilder<T extends string> {
-  (pattern: T, params?: HrefBuilderParams<T>, searchParams?: HrefBuilderSearchParams<T>): string;
+interface HrefBuilder<T extends RoutePattern> {
+  <P extends HrefBuilderPattern<T>>(
+    pattern: P['output'],
+    params?: HrefBuilderParams<Extract<T, { source: P['source'] }>>,
+    searchParams?: HrefBuilderSearchParams<Extract<T, { source: P['source'] }>>,
+  ): string;
 }
 
-export function createHrefBuilder<T extends RoutePattern>(
-  pattern?: T,
-): HrefBuilder<HrefBuilderPattern<T>>;
+export function createHrefBuilder<T extends RoutePattern>(pattern?: T): HrefBuilder<T>;
 export function createHrefBuilder<T extends ReadonlyArray<RoutePattern>>(
   patterns?: T,
-): HrefBuilder<HrefBuilderPattern<T[number]>>;
+): HrefBuilder<T[number]>;
 export function createHrefBuilder(patterns: any): any {
   return (pattern: string, params?: any, searchParams?: any) => {
     return ''; // TODO
